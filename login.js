4c66cf021299720717c6c2d8e28eb5eb98bf7b4f
unknown <sophie.sa43@gmail.com>
2022-01-26 16:14:04 +0200

    added alert

--- login.js
+++ login.js
@@ -1,21 +1,13 @@
-//var name = [];
-//var email = [];
-//var password = [] ;
+function submit(){
+    var email = document.getElementById("email"); 
+    var password = document.getElementById("password"); 
+    //check empty password field  
+    if(email == "" && password == "") {
+        window.location.href="home.html";
 
-function submit() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-
-    if(email == "" && password =="") {
-     alert('Please provide a valid email address');
-     email.focus;
-    return false;
     }
-    if(login) {
-        window.location.href = "home.html"
-    }
+     else{
+
+        alert("Passwords did not match") 
+    } 
 }
-
-
-
