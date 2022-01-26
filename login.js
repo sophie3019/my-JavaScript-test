90c3fbc433541e5744f76a9bb7155690a4e5205b
unknown <sophie.sa43@gmail.com>
2022-01-26 10:53:56 +0200

    added bootstrap

---
+++ login.js
@@ -0,0 +1,19 @@
+
+
+function submit() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+
+    if(email == "" && password =="") {
+     alert('Please provide a valid email address');
+     email.focus;
+    return false;
+    }
+    if(login) {
+        window.location.href = "home.html"
+    }
+}
+
+
+
