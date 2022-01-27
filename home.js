55bb3480e4dde6a8b9ae6264c7810f36bb43a170
unknown <sophie.sa43@gmail.com>
2022-01-27 15:02:34 +0200

    added const

--- home.js
+++ home.js
@@ -1,6 +1,3 @@
 const submit = ["Sophie","34","Seline","55","Charity","19","Kagisho","25","Mdu","45","Hardy","34","Sithembiso","29","Precious","34","Happy","22","Vusi","27","Ansiwie","94","Lebo","18","MT","21","Khu","33","Lucky","45"];
 
-for (let x =0; x < submit.length; x++) {
-document.getElementById("test").innerHTML += "<p>" + submit[x] + "</p>";
 
-}
