1a94f20b4313cec6196bc9a4e929c8d00b4aa860
unknown <sophie.sa43@gmail.com>
2022-01-27 14:47:55 +0200

    added style css

--- login.js
+++ login.js
@@ -1,13 +1,16 @@
-function submit(){
-    var email = document.getElementById("email"); 
-    var password = document.getElementById("password"); 
+function submit() {
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
     //check empty password field  
-    if(email == "" && password == "") {
-        window.location.href="home.html";
+
+        if (email = "user@gmail.com" && password == "mypassword") {
+            window.location.href = "home.html";
 
     }
-     else{
+    else {
+        alert("Passwords did not match");
+        return;
 
-        alert("Passwords did not match") 
-    } 
+    }
 }
